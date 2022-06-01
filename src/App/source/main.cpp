0c5fb5c114ae829ee3b28a6d37f47abe2e2e4a7f
ImGili <18017158729@163.com>
2022-06-01 17:53:18 +0800

    🎉 init: init proj

--- src/App/source/main.cpp
+++ src/App/source/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <QApplication>
+#include <QGraphicsView>
 #include "Core/test.h"
+#include "test.h"
 int main(int argc, char *argv[])
 {
     test();
-    std::cout << "hello world!" << std::endl;
+    QApplication* app = new QApplication(argc, argv);
+    QGraphicsScene scene;
+    scene.addText("Hello, world!");
+    QGraphicsRectItem* rect = scene.addRect(0, 0, 100, 100);
+
+    View view;
+    view.setScene(&scene);
+    view.show();
+    app->exec();
 }
